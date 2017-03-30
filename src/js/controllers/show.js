38a273314756b47b14e3773f06e9d9d392142946
Joshua Bradford <jjyet@yahoo.com>
2017-03-30 09:57:51 -0400

    added working modal with no info yet

--- src/js/controllers/show.js
+++ src/js/controllers/show.js
@@ -14,6 +14,15 @@ function ShowController ($scope, $http, SERVER, $state, $cookies, $rootScope) {
   $scope.info = $rootScope.shows[chosenShow];
   $scope.fullUrl = "http://image.tmdb.org/t/p/w650//" + $rootScope.shows[chosenShow].backdrop_path;
   //console.log($scope.fullUrl)
+
+
+  $scope.modal = function (){
+    $scope.test = "is-active";
+  }
+
+  $scope.close = function (){
+    $scope.test = "";
+  }
 }
 
 ShowController.$inject = ['$scope', '$http', 'SERVER', '$state', '$cookies', '$rootScope'];
