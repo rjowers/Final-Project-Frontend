4aa6205796a947484f5daad24d7a04b1c920af7b
Ryan Jowers <rjowers@g.clemson.edu>
2017-04-01 22:34:35 -0400

    did some stuff with the profile controller

--- src/js/controllers/profile-page.js
+++ src/js/controllers/profile-page.js
@@ -1,15 +1,24 @@
-function ProfilePageController ($scope, $http, SERVER, $state, $cookies, $rootScope, AccountService) {
+function ProfilePageController ($scope, $http, SERVER, $state, $cookies, $rootScope, $stateParams, AccountService) {
 
-  $http.get(`${SERVER}/users/`).then(resp => {
+  // $http.get(`${SERVER}/users/`).then(resp => {
+  //     console.log(resp.data)
+  //     // console.log(resp.data[5].User.profileUrl)
+  //     //console.log(resp.data[0].review)
+  //     $scope.test= resp.data;
+  //   });
+
+  $http.get(`${SERVER}/showreviews/${$cookies.get('clickedPhoto')}`).then(resp => {
       console.log(resp.data)
-      // console.log(resp.data[5].User.profileUrl)
-      //console.log(resp.data[0].review)
-      $scope.test= resp.data;
+      $scope.test3 = resp.data;
     });
+
+
+
+
 
 
 }
 
-ProfilePageController.$inject = ['$scope', '$http', 'SERVER', '$state', '$cookies', '$rootScope', 'AccountService'];
+ProfilePageController.$inject = ['$scope', '$http', 'SERVER', '$state', '$cookies', '$rootScope', '$stateParams', 'AccountService'];
 
 export default ProfilePageController;
