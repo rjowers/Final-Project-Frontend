776f6664e7dd8368744a78a4468801b55f00371b
Joshua Bradford <jjyet@yahoo.com>
2017-04-05 20:28:12 -0400

    got better search page with info on page

--- src/js/controllers/menu.js
+++ src/js/controllers/menu.js
@@ -3,19 +3,14 @@ import { token } from "../token"
 function menuController ($scope, $http, SERVER, $state, $cookies, $rootScope, AccountService, $location, $window, $stateParams ) {
 
   $scope.submit = function(data){
-    console.log(data)
     searchShow(data.text);
   }
-
-
 
   function searchShow (searchInput) {
     $http.get(`https://api.themoviedb.org/3/search/tv?api_key=${token}&query=${searchInput}`)
     .then ( resp => {
-      $scope.searchResults = resp.data.results;
+      $rootScope.searchResults = resp.data.results;
       console.log(resp.data.results)
-      $scope.test = "fred"
-      // $location.url()
       //$window.location.href = `#!/show/${resp.data.results[0].id}`;
       //$window.location.href = `#!/results`;
       $state.go('results')
@@ -38,10 +33,10 @@ function menuController ($scope, $http, SERVER, $state, $cookies, $rootScope, Ac
     .then(resp => {
       //console.log(resp.data.id)
       $scope.userId = resp.data.id
-      console.log($scope.userId);
+      //console.log($scope.userId);
       $http.get(`${SERVER}/userreviews/${$scope.userId}`).then(resp => {
         $scope.user = resp.data;
-        console.log(resp.data)
+        //console.log(resp.data)
         $scope.profileUrl = resp.data[0].User.profileUrl;
         // console.log($scope.profileUrl);
         //
@@ -50,54 +45,6 @@ function menuController ($scope, $http, SERVER, $state, $cookies, $rootScope, Ac
     .catch(error => {
           console.log(error);
     })
-
-
-
-
-
-// console.log('here')
-// console.log(AccountService.token())
-//
-//   $http.get(`${SERVER}/me`,{
-//     headers: AccountService.token()
-//   }).then(resp=>{
-//     console.log(resp)
-//   })
-
-  // $http.post(`${SERVER}/comments`, data, {
-  //   headers: AccountService.token()
-  // }).then(resp => {
-  //   console.log(resp);
-  // });
-
-
-
-/*
-var token = req.headers[‘access-token’] || req.query.access_token;
-         // If they didn’t provide a token, send them away.
-         if (!token) {
-           return res.status(401).send({
-             message: “Must be authenticated to use this route.”
-           });
-         }
-
-         // Try to decode the token.
-         try {
-           var decoded = jwt.decode(token, appSecrets.jwtSecret);
-           var userId = decoded.id;
-           // If decoding the token was successful,
-           // look up the user from the token Id.
-           User.findById(userId).then(user => {
-
-             // If no matching user was found, send them away.
-             if (!user) {
-               return res.status(401).send({ message: “Error during authentication.” });
-             }
-            */
-
-
-// function ClickedProfile (){
-// }
 };
 
   menuController.$inject = ['$scope', '$http', 'SERVER', '$state', '$cookies', '$rootScope', 'AccountService', '$location', '$window', '$stateParams'] ;
